cf3b6aa0a608140c30eebc1b4d27425ea592155f
AasifNazir786 <aasif.n.s786@gmail.com>
2024-09-30 19:30:26 +0530

    implemented DBUtil class and create a connection to a database

--- src/com/example/utilities/DBUtil.java
+++ src/com/example/utilities/DBUtil.java
@@ -1,5 +1,24 @@
 package com.example.utilities;
 
-public class DBUtil
-{
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+
+    private static String URL = "jdbc:postgresql://localhost:5432/jdbc";
+    private static String USER = "postgres";
+    private static String PASSWORD = "njasm786";
+
+    static{
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
